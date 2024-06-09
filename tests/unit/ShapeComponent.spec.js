906257e357e0842b8ea20333d47ce797607d39d0
Hristiyan Dimitrov <hd@Hristiyans-MacBook-Pro.local>
2024-06-09 08:22:57 +0300

    add-shape-component-tests

--- tests/unit/ShapeComponent.spec.js
+++ tests/unit/ShapeComponent.spec.js
@@ -1,7 +1,7 @@
 import { mount } from "@vue/test-utils";
 import ShapeCard from "@/components/ShapeCard.vue";
 
-describe("Check if shape title is rendered", () => {
+describe("Check if Shape Component renders and works correctly", () => {
   test("Check if shape title is rendered", () => {
     const wrapper = mount(ShapeCard, {
       props: {
@@ -13,31 +13,42 @@ describe("Check if shape title is rendered", () => {
     });
     expect(wrapper.text()).toContain("Triangle");
   });
-});
+  test("Check if click adds border", async () => {
+    const wrapper = mount(ShapeCard, {
+      props: {
+        shape: { Circle: { width: "100px", height: "100px", borderRadius: "50%" } },
+        index: 0,
+        isActive: false,
+      },
+      global: {
+        provide: {
+          activeIndex: {
+            value: 0,
+          },
+        },
+      },
+    });
+    expect(wrapper.classes()).not.toContain("border-sky-300");
+    await wrapper.trigger("click");
+    expect(wrapper.classes()).toContain("border-sky-300");
+  });
 
-// it("sets activeIndex on click", async () => {
-//   const wrapper = mount(ShapeCard, {
-//     props: {
-//       shape: { Circle: { width: "100px", height: "100px", borderRadius: "50%" } },
-//       index: 0,
-//       isActive: false,
-//     },
-//     global: {
-//       provide: {
-//         activeIndex: {
-//           value: 0,
-//         },
-//       },
-//     },
-//   });
-//   // Initially, the activeIndex should not be set
-//   expect(wrapper.classes()).not.toContain("border-sky-300");
-//   // Simulate click event
-//   await wrapper.trigger("click");
-//   await nextTick();
-//   // Check if the click event sets the activeIndex
-//   expect(wrapper.emitted("update:modelValue")[0][0]).toBe(0);
-//   // Check if the class changes
-//   expect(wrapper.classes()).toContain("border-sky-300");
-// }
-// );
+  test("Check if click updates active index", async () => {
+    const wrapper = mount(ShapeCard, {
+      props: {
+        shape: { Circle: { width: "100px", height: "100px", borderRadius: "50%" } },
+        index: 0,
+        isActive: true,
+      },
+      global: {
+        provide: {
+          activeIndex: {
+            value: 0,
+          },
+        },
+      },
+    });
+    await wrapper.trigger("click");
+    expect(wrapper.emitted("update:modelValue")[0][0]).toBe(0);
+  });
+});
