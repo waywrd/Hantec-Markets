77dfa9429d6024d19184f80d940f40de323978dc
Hristiyan Dimitrov <hd@Hristiyans-MacBook-Pro.local>
2024-06-09 07:28:56 +0300

    move-layout-folder-2

---
+++ src/layout/BasicLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full h-screen">
+    <NavigationHeader />
+    <div class="w-full h-full flex justify-center"><slot></slot></div>
+  </div>
+</template>
+<script setup>
+import NavigationHeader from "./navigation/NavigationHeader.vue";
+</script>
